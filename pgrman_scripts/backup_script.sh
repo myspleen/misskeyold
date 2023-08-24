0000486ae893ea392cb84e2612604acadc142eb9
myspleen <from.my.spleen@gmail.com>
2023-08-25 03:07:19 +0900

    #fix

--- pgrman_scripts/backup_script.sh
+++ pgrman_scripts/backup_script.sh
@@ -2,19 +2,13 @@
 
 export TZ='Asia/Tokyo'
 
-BASE_BACKUP_DIR="/var/lib/postgresql/backup"
-DATE=$(date +"%Y-%m-%d_%H%M%S")
-MODE="$1" # "full" or "incremental"
-BACKUP_SUBDIR="${BASE_BACKUP_DIR}/${DATE}_$MODE"
+BACKUP_DIR="/var/lib/postgresql/backup"
+DB_DIR="/var/lib/postgresql/data"
 ARCHIVE_DIR="/var/lib/postgresql/archive"
-RCLONE_REMOTE="onedrive"
-RCLONE_PATH="server/backup/misskey"
+MODE="$1"
+RCLONE_REMOTE="onedrive"  # rcloneリモート名
+RCLONE_PATH="server/backup/misskey"  # 保存先のOneDriveフォルダのパス
 LOG_FILE="/var/lib/postgresql/backup/backup.log"
-
-# rcloneの設定ファイルの場所を指定
-export RCLONE_CONFIG="/root/.config/rclone/rclone.conf"
-export PGUSER=$POSTGRES_USER
-export PGDATABASE=$POSTGRES_DB
 
 # Line通知スクリプト
 send_line_message() {
@@ -31,13 +25,17 @@ send_line_message() {
         }' https://api.line.me/v2/bot/message/push 2>&1)
 }
 
-# Ensure backup and archive directories exist
-for dir in "$BACKUP_SUBDIR" "$ARCHIVE_DIR"; do
+for dir in "$BACKUP_DIR" "$ARCHIVE_DIR"; do
     if [ ! -d "$dir" ]; then
         mkdir -p "$dir"
         chown postgres:postgres "$dir"
     fi
 done
+
+# rcloneの設定ファイルの場所を指定
+export RCLONE_CONFIG="/root/.config/rclone/rclone.conf"
+export PGUSER=$POSTGRES_USER
+export PGDATABASE=$POSTGRES_DB
 
 # Ensure log directory exists
 if [ ! -d "$(dirname $LOG_FILE)" ]; then
@@ -52,38 +50,31 @@ log() {
 log "Backup script started."
 
 # pg_rmanバックアップを実行
-/usr/lib/postgresql/15/bin/pg_rman backup --backup-mode=$MODE -B $BASE_BACKUP_DIR -D $DB_DIR -A $ARCHIVE_DIR >> $LOG_FILE 2>&1
+/usr/lib/postgresql/15/bin/pg_rman backup --backup-mode=$MODE -B $BACKUP_DIR -D $DB_DIR -A $ARCHIVE_DIR >> $LOG_FILE 2>&1
 if [ $? -ne 0 ]; then
     log "Error: pg_rman backup failed."
     send_line_message "❌Misskey - Error: pg_rman backup failed."
     exit 1
 fi
+log "pg_rman backup --backup-mode=$MODE -b $BACKUP_DIR -D $DB_DIR -A $ARCHIVE_DIR finished."
 
 # バックアップを検証
-/usr/lib/postgresql/15/bin/pg_rman validate -B $BACKUP_SUBDIR -D $DB_DIR -A $ARCHIVE_DIR >> $LOG_FILE 2>&1
+/usr/lib/postgresql/15/bin/pg_rman validate -B $BACKUP_DIR -D $DB_DIR -A $ARCHIVE_DIR >> $LOG_FILE 2>&1
 if [ $? -ne 0 ]; then
     log "Error: pg_rman validate failed."
     send_line_message "❌Misskey - Error: pg_rman validate failed."
     exit 1
 fi
-
-# 圧縮
-COMPRESSED_BACKUP="${BACKUP_SUBDIR}.tar.gz"
-log "Compressing the backup directory."
-tar cf - $BACKUP_SUBDIR | pigz > $COMPRESSED_BACKUP
-if [ $? -ne 0 ]; then
-    log "Error: Compression using pigz failed."
-    send_line_message "❌Misskey - Error: Compression using pigz failed."
-    exit 1
-fi
+log "/usr/lib/postgresql/15/bin/pg_rman validate -b $BACKUP_DIR -D $DB_DIR -A $ARCHIVE_DIR finished."
 
 # rcloneでOneDriveにアップロード
-rclone copy $COMPRESSED_BACKUP $RCLONE_REMOTE:$RCLONE_PATH >> $LOG_FILE 2>&1
+rclone sync $BACKUP_DIR $RCLONE_REMOTE:$RCLONE_PATH >> $LOG_FILE 2>&1
 if [ $? -ne 0 ]; then
     log "Error: rclone sync failed."
     send_line_message "❌Misskey - Error: rclone sync failed."
     exit 1
 fi
+log "rclone sync $BACKUP_DIR $RCLONE_REMOTE:$RCLONE_PATH finished."
 
 log "Backup script completed."
 
@@ -92,6 +83,3 @@ if [ "$MODE" == "full" ]; then
 else
     send_line_message "✅Misskey - Incremental backup completed."
 fi
-
-# Cleanup 
-rm -rf "$BACKUP_SUBDIR"
