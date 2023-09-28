60fa9ac4dc5141025805f06e5eeacecbd8914273
myspleen <from.my.spleen@gmail.com>
2023-09-28 14:13:59 +0900

    add

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -394,8 +394,8 @@ interface Watcher {
  * 常にメモリにロードしておく必要がないような設定情報を保管するストレージ(非リアクティブ)
  */
 import { miLocalStorage } from './local-storage';
-import lightTheme from '@/themes/l-light.json5';
-import darkTheme from '@/themes/d-green-lime.json5';
+import lightTheme from '@/themes/estampie-light.json5';
+import darkTheme from '@/themes/estampie-dark.json5';
 
 export class ColdDeviceStorage {
 	public static default = {
